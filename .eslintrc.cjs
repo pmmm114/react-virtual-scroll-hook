80ce8b5d877bd98d29faed75bb50a83dc46cc1ba
LeeKangbok <pmmm114@gmail.com>
2025-01-07 18:46:17 +0900

    setting: 프로젝트 개발환경 추가 및 변경
    - Node버전관리를 위한 nvmrc. 추가
    - ESLint
      - TypeScript와 React, Prettier를 위한 설정 추가
    - Prettier
    - Vite
      - Removed webpack configuration
    - 의존성 추가에 따른 package.json 업데이트
    - .gitignore 업데이트
    - Yarn(+4)로 전환

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+  plugins: ['import', 'react-refresh'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.app.json',
+      },
+    },
+  },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
